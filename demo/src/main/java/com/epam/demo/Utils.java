8fd91e6ab7e0c8ab66369ac9d00eeb14a765cfc4
Davidchanz <tovliskatsi@gmail.com>
2023-07-31 16:49:12 +0400

    Build Tools Task 1 completed

--- demo/src/main/java/com/epam/demo/Utils.java
+++ demo/src/main/java/com/epam/demo/Utils.java
@@ -1,9 +1,15 @@
 package com.epam.demo;
 
+import com.epam.utils.StringUtils;
+
 import java.util.List;
 
 public class Utils {
     public static boolean isAllPositiveNumbers(List<String> args) {
         //magic happens here
+        for(var s : args)
+            if(!StringUtils.isPositiveNumber(s))
+                return false;
+        return true;
     }
 }
