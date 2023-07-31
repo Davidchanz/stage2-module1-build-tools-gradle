5e10b9f7d4a6d023fc8e9bc186feaea858ef7849
Davidchanz <tovliskatsi@gmail.com>
2023-07-31 17:10:43 +0400

    Build Tools Task 1 completed
    Add isCreatable check

--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -2,7 +2,10 @@ package com.epam.utils;
 
 public class StringUtils {
     public static boolean isPositiveNumber(String str) {
+        if(org.apache.commons.lang3.math.NumberUtils.isCreatable(str))
          //here magic will happen
-        return org.apache.commons.lang3.math.NumberUtils.toDouble(str) > 0;
+            return org.apache.commons.lang3.math.NumberUtils.toDouble(str) > 0.0;
+        else
+            return false;
     }
 }
